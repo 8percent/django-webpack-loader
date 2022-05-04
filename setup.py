2273b99f3903743907500f6d369deeb715ca5272
Bruno Alla <alla.brunoo@gmail.com>
2022-05-04 17:25:56 +0900

    Add support for Django 4.0
    (cherry picked from commit 72a63804d8694d7b12a0d87efeaf65dc6b7f4f69)

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ def rel(*parts):
     '''returns the relative path to a file wrt to the current directory'''
     return os.path.abspath(os.path.join(os.path.dirname(__file__), *parts))
 
-README = open('README.md', 'r').read()
+with open('README.md', 'r') as handler:
+  README = handler.read()
 
 with open(rel('webpack_loader', '__init__.py')) as handler:
-    INIT_PY = handler.read()
+  INIT_PY = handler.read()
 
 
 VERSION = re.findall("__version__ = '([^']+)'", INIT_PY)[0]
@@ -25,16 +26,23 @@ setup(
   long_description_content_type="text/markdown",
   author = 'Owais Lone',
   author_email = 'hello@owaislone.org',
-  download_url = 'https://github.com/owais/django-webpack-loader/tarball/{0}'.format(VERSION),
-  url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
+  download_url = 'https://github.com/django-webpack/django-webpack-loader/tarball/{0}'.format(VERSION),
+  url = 'https://github.com/django-webpack/django-webpack-loader', # use the URL to the github repo
   keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
   classifiers = [
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Framework :: Django',
+    'Framework :: Django :: 2.0',
+    'Framework :: Django :: 2.1',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django :: 3.1',
+    'Framework :: Django :: 3.2',
+    'Framework :: Django :: 4.0',
     'Environment :: Web Environment',
     'License :: OSI Approved :: MIT License',
   ],
